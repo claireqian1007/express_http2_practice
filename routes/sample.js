b075e53f3c8263c124c4c9a253d6cfa9c0cd80c9
= <claire_qian@epam.com>
2023-03-24 18:04:01 +0800

    chore: add prefetch because server push not support

--- routes/sample.js
+++ routes/sample.js
@@ -1,24 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
+var path = require("path");
 
-router.get('/', (req, res) => {
-  res.push(['/js/bar.js', '/stylesheets/bar.css'], express.static(path.join(__dirname, 'public')))
+router.get("/", (req, res) => {
   res.send(`
       <!DOCTYPE html>
       <html lang="en">
           <head>
-              <link rel="stylesheet" href="/stylesheets/bar.css">
+              <link rel="preload" as="style" href="https://localhost:3000/stylesheets/bar.css" >
+              <link rel="preload" as="script" href="https://localhost:3000/js/bar.js" >
+
+              <link rel="stylesheet" href="https://localhost:3000/stylesheets/bar.css" >
               <meta charset="UTF-8">
               <title>Bar Document</title>
+             
           </head>
           <body>
               <h1>Sample Page</h1>
               <p>This is a bar document.</p>
               <p id="demo"></p>
-              <script src="/js/bar.js"></script>
+              <script src="https://localhost:3000/js/bar.js"></script>
           </body>
       </html>`);
-})
+});
 
 module.exports = router;
